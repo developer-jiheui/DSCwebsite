05e614fcd8e862464203a5c2b877232a751dd31c
jihee <developer.jiheuilee@gmail.com>
2021-02-10 19:38:30 -0800

    started to work on Multicolor Animation

--- frontend/src/components/Featured/index.js
+++ frontend/src/components/Featured/index.js
@@ -1,11 +1,16 @@
 import React from "react";
 import {Container, Image} from 'semantic-ui-react';
+import MulticolorAnimation from "../MulticolorAnimation";
+import "./index.css";
 
 const Featured = () => {
     return <Container>
-            <Image 
-                src='images/CoverPhoto-FullColor.png' 
-                fluid />
+            <MulticolorAnimation></MulticolorAnimation>
+            <div id="featuredImageContainer" style={{backgroundImage: 'url(./images/CoverPhoto-Plain.png'}}>
+       
+
+            </div>
+            
             </Container>;
 }
 
---
+++ frontend/src/components/MulticolorAnimation/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {Container, Image} from 'semantic-ui-react';
+import "./index.css";
+
+const MulticolorAnimation = () => {
+    return <div id="multicolorAnimation" >
+            </div>;
+            
+            
+}
+
+export default MulticolorAnimation;
