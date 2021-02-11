31cbb9cc75982fe2e5f8bd10988c160323ff79d2
melj <m3lissa.johnson@hotmail.com>
2021-02-10 16:07:57 -0800

    Partial for home page

---
+++ frontend/src/components/Featured/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {Container, Image} from 'semantic-ui-react';
+
+const Featured = () => {
+    return <Container>
+            <Image 
+                src='images/CoverPhoto-FullColor.png' 
+                fluid />
+            </Container>;
+}
+
+export default Featured;
--- frontend/src/pages/Homepage/index.js
+++ frontend/src/pages/Homepage/index.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Grid } from "semantic-ui-react";
+import Featured from "../../components/Featured";
 import Footer from "../../components/Footer";
 
 import Navbar from "../../components/Navbar";
@@ -8,9 +10,10 @@ const Homepage = () => {
   return (
     <div>
       <Navbar></Navbar>
+      <Featured></Featured>
       <Footer></Footer>
     </div>
-    )
+  )
 };
 
 export default Homepage;
