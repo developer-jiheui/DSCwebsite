342705d9220dd75d7aece80af0b0fe51b8dc42db
Guilherme Eckert <guilherme-sonnemann@hotmail.com>
2021-02-13 16:31:32 -0800

    add link to event

--- frontend/src/pages/event/index.js
+++ frontend/src/pages/event/index.js
@@ -16,7 +16,7 @@ const Events = () => {
       <Item.Image size='medium' circular src='https://curiocity.com/edmonton/wp-content/uploads/2020/09/canada_games_council_niagara_2021_canada_summer_games_postponed.jpg' />
 
       <Item.Content>
-        <Item.Header as='a'>Douglas College</Item.Header>
+      <Link to="/eventfeed">  <Item.Header as='a'>Douglas College</Item.Header> </Link>
         <Item.Meta> Lorem, Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam quo beatae illo laudantium repellat. Ipsum, minus natus perspiciatis vitae laborum possimus cupiditate animi nesciunt a alias esse id voluptates eaque. ipsum dolor sit amet consectetur adipisicing elit. Odio perspiciatis amet pariatur iusto dolorem atque sequi, sed facilis consectetur enim vero, harum, nobis ea commodi incidunt voluptatem ullam nemo distinctio? </Item.Meta>
     
         <Item.Extra className="details" >Additional Details</Item.Extra>
@@ -27,7 +27,7 @@ const Events = () => {
       <Item.Image rounded  size='medium' circular src='https://images.dailyhive.com/20180625114454/shutterstock_198202814.jpg' />
 
       <Item.Content>
-        <Item.Header as='a'>Douglas College</Item.Header>
+      <Link to="/eventfeed">  <Item.Header as='a'>Douglas College</Item.Header> </Link>
         <Item.Meta> Lorem, Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, nobis cumque adipisci ullam ea id! Minus voluptas distinctio dicta modi, adipisci expedita commodi aliquid hic, unde, fugit numquam necessitatibus maiores. ipsum dolor sit amet consectetur adipisicing elit. Odio perspiciatis amet pariatur iusto dolorem atque sequi, sed facilis consectetur enim vero, harum, nobis ea commodi incidunt voluptatem ullam nemo distinctio? </Item.Meta>
          <Item.Extra className="details">Additional Details</Item.Extra>
       </Item.Content>
