991e0a43c3f5fb7dc337ab56d81f85ced4099a58
melj <m3lissa.johnson@hotmail.com>
2021-02-23 16:15:55 -0800

    added back the card stuff to homepage since cardComponent is deleted now, and it was causing errors. and temporily making overflow hiddden on the root elem so we can test responsiveness better. until the hamburger comes

--- frontend/src/pages/Homepage/index.js
+++ frontend/src/pages/Homepage/index.js
@@ -10,7 +10,6 @@ import {
 import { Link } from "react-router-dom";
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
-import CardComponent from "../../components/CardComponent";
 
 import "./index.css";
 
@@ -29,84 +28,102 @@ const Homepage = () => {
           <Grid.Column width={10} id="events-container">
             <h1>Events</h1>
             <Card.Group>
-              <CardComponent title="Title" date="February 19th, 2020">
-                <Grid>
-                  <Grid.Column width={6}>
-                    <Image
-                      src="https://react.semantic-ui.com/images/wireframe/image.png"
-                      size="small"
-                    />
-                  </Grid.Column>
-                  <Grid.Column width={10}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                    occaecat cupidatat non proident, sunt in culpa qui officia
-                    deserunt mollit anim id est laborum.
-                  </Grid.Column>
-                </Grid>
-                <Divider></Divider>
-                <Card.Content extra>
-                  <i className="calendar alternate outline icon"></i>
-                  February 19th, 2020
-                  <Link to={"/events"}>
-                    <Button className="event-more-button">See More</Button>
-                  </Link>
+              <Card className="card" fluid>
+                <Card.Content>
+                  <Card.Header>Title</Card.Header>
+                  <Divider></Divider>
+                  <Card.Description>
+                    <Grid>
+                      <Grid.Column width={6}>
+                        <Image
+                          src="https://react.semantic-ui.com/images/wireframe/image.png"
+                          size="small"
+                        />
+                      </Grid.Column>
+                      <Grid.Column width={10}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                        do eiusmod tempor incididunt ut labore et dolore magna
+                        aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                        ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        Duis aute irure dolor in reprehenderit in voluptate velit
+                        esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                        occaecat cupidatat non proident, sunt in culpa qui officia
+                        deserunt mollit anim id est laborum.
+                      </Grid.Column>
+                    </Grid>
+                    <Divider></Divider>
+                    <Card.Content extra>
+                      <i className="calendar alternate outline icon"></i>
+                      February 19th, 2020
+                      <Link to={"/events"}>
+                        <Button className="event-more-button">See More</Button>
+                      </Link>
+                    </Card.Content>
+                  </Card.Description>
                 </Card.Content>
-              </CardComponent>
-              <CardComponent title="Title" date="February 19th, 2020">
-                <Grid>
-                  <Grid.Column width={6}>
-                    <Image
-                      src="https://react.semantic-ui.com/images/wireframe/image.png"
-                      size="small"
-                    />
-                  </Grid.Column>
-                  <Grid.Column width={10}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                    occaecat cupidatat non proident, sunt in culpa qui officia
-                    deserunt mollit anim id est laborum.
-                  </Grid.Column>
-                </Grid>
-                <Divider></Divider>
-                <Card.Content extra>
-                  <i className="calendar alternate outline icon"></i>
-                  February 19th, 2020
-                  <Link to={"/events"}>
-                    <Button className="event-more-button">See More</Button>
-                  </Link>
+              </Card>
+              <Card className="card" fluid>
+                <Card.Content>
+                  <Card.Header>Title</Card.Header>
+                  <Divider></Divider>
+                  <Card.Description>
+                    <Grid>
+                      <Grid.Column width={6}>
+                        <Image
+                          src="https://react.semantic-ui.com/images/wireframe/image.png"
+                          size="small"
+                        />
+                      </Grid.Column>
+                      <Grid.Column width={10}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                        do eiusmod tempor incididunt ut labore et dolore magna
+                        aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                        ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        Duis aute irure dolor in reprehenderit in voluptate velit
+                        esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                        occaecat cupidatat non proident, sunt in culpa qui officia
+                        deserunt mollit anim id est laborum.
+                      </Grid.Column>
+                    </Grid>
+                    <Divider></Divider>
+                    <Card.Content extra>
+                      <i className="calendar alternate outline icon"></i>
+                      February 19th, 2020
+                      <Link to={"/events"}>
+                        <Button className="event-more-button">See More</Button>
+                      </Link>
+                    </Card.Content>
+                  </Card.Description>
                 </Card.Content>
-              </CardComponent>
+              </Card>
             </Card.Group>
           </Grid.Column>
           <Grid.Column width={6} id="news-container">
             <h1>News</h1>
             <Card.Group>
-              <CardComponent title="COVID-19 Update" date="February 19th, 2020">
-                <Image
-                  size="tiny"
-                  centered
-                  src="https://react.semantic-ui.com/images/avatar/large/matthew.png"
-                  wrapped
-                  ui={false}
-                />
+              <Card className="card" fluid>
                 <Card.Content>
-                  <br></br>
+                  <Card.Header>Title</Card.Header>
+                  <Divider></Divider>
                   <Card.Description>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua.
+                    <Image
+                      size="tiny"
+                      centered
+                      src="https://react.semantic-ui.com/images/avatar/large/matthew.png"
+                      wrapped
+                      ui={false}
+                    />
+                    <Card.Content>
+                      <br></br>
+                      <Card.Description>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                        do eiusmod tempor incididunt ut labore et dolore magna
+                        aliqua.
+                      </Card.Description>
+                    </Card.Content>
                   </Card.Description>
                 </Card.Content>
-              </CardComponent>
+              </Card>
             </Card.Group>
           </Grid.Column>
         </Grid>
