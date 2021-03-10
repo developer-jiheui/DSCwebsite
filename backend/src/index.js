d50201cec7ae771f299783f710973f37ce80d7a2
leandrofahur <fahurleandro@gmail.com>
2021-03-10 14:32:34 -0800

    mongoose setup to handle the connection with mongoDB

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,16 @@
 const express = require("express");
+const connectDB = require("../config/db");
+
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+// Connection with database:
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("Hello from Express.js");
 });
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, (req, res) => {
   console.log(`Server up and running on port ${PORT}`);
 });
