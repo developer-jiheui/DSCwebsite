504e269795cc3ca84a12c273f97ffa9d7e93aa55
leandrofahur <fahurleandro@gmail.com>
2021-03-12 15:19:41 -0800

    simple post example without mongoDB connection

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const connectDB = require("../config/db");
 
 const app = express();
+app.use(express.json({ extended: false }));
 
 // Connection with database:
 connectDB();
--- backend/src/routes/users/index.js
+++ backend/src/routes/users/index.js
@@ -1,15 +1,44 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
 
 /*
  * @route   GET /users
  * @desc    User routing test for the api
  * @access  Public
  */
-router.get("/", (req, res) => {
-  //   res.send(req.body);
-  console.log(req.body);
-  res.send("Request Received");
-});
+router.post(
+  "/",
+  [
+    check("firstName", "First name is required").not().isEmpty(),
+    check("lastName", "Last name is required").not().isEmpty(),
+    check("email", "Please include a valid email").not().isEmpty(),
+  ],
+  async (req, res) => {
+    // check validation and return an object array with the failed checks:
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    // deconstructor (just use the name of the variable you are receiving from )
+    const { firstName, lastName, email } = req.body;
+
+    // try-catch block for mongoDB:
+    try {
+      res.status(200).send({
+        firstName,
+        lastName,
+        email,
+      });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server error!");
+    }
+
+    // console.log(req.body);
+    // res.send("Request Received");
+  }
+);
 
 module.exports = router;
