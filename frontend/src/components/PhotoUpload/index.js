9b91d7957a7242ac2822e10d59414bbf658e0fac
melj <m3lissa.johnson@hotmail.com>
2021-03-17 18:20:11 -0700

    fix for photoupload, default profile pic if none chosen

--- frontend/src/components/PhotoUpload/index.js
+++ frontend/src/components/PhotoUpload/index.js
@@ -6,7 +6,7 @@ import './index.css';
 const defaultProfilePicURL = "https://media.istockphoto.com/vectors/default-profile-picture-avatar-photo-placeholder-vector-illustration-vector-id1214428300?k=6&m=1214428300&s=612x612&w=0&h=rvt5KGND3z8kfrHELplF9zmr8d6COZQ-1vYK9mvSxnc=";
 
 const PhotoUploader = ({src}) => {
-    const [fileImage, setFileImage] = useState(src);
+    const [fileImage, setFileImage] = useState(src || defaultProfilePicURL);
 
     const handleFileChange = (e) => {
         setFileImage(URL.createObjectURL(e.target.files[0]));
