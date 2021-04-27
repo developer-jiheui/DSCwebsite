af74d100be6dd4c9163025da0c032324cacfc07d
melj <m3lissa.johnson@hotmail.com>
2021-04-26 17:10:48 -0700

    implemented detelete and update POST routes, for news and events

--- backend/src/model/Post.js
+++ backend/src/model/Post.js
@@ -1,19 +1,10 @@
 const mongoose = require("mongoose");
 
 const PostSchema = new mongoose.Schema({
-  //   _id: {
-  //   type: String,
-  //   required: true,
-  // },
   post_type: {
     type: String,
     required: true
-  },
-  // seq: { type: Number, default: 0 },
-  //   post_type: {
-  //   type: String,
-  //   required: true,
-  // }
+  }
 });
 
 module.exports = Post = mongoose.model("post", PostSchema);
--- backend/src/routes/posts/index.js
+++ backend/src/routes/posts/index.js
@@ -7,42 +7,62 @@ const News = require("../../model/News");
 const config = require("config");
 
 /*
- * @route   POST /posts/events
- * @desc    Post routing to get Events
+ * @route   GET /posts/events
+ * @desc    Posts routing to get all Events
  * @access  Public
  */
 router.get("/events", async (req, res) => {
     try {
         // fetch posts based on post type
-        let posts = await Event.find();                
+        let posts = await Event.find();
         res.status(200).json({
-            message: `Retrived ${posts.length} posts of type ${post_type}`,
+            message: `Retrived ${posts.length} posts of type Event`,
             data: posts
         });
-    } catch(error) {
+    } catch (error) {
         return res.status(500).send("An error occured retrieving posts!");
     }
 });
 
 /*
- * @route   POST /posts/news
- * @desc    Post routing to get News
+ * @route   GET /posts/events/:id
+ * @desc    Posts routing to get a single Event
+ * @access  Public
+ */
+
+// TODO: implement route for single event find
+
+/*
+ * @route   GET /posts/news
+ * @desc    Posts routing to get all News
  * @access  Public
  */
 router.get("/news", async (req, res) => {
     try {
         // fetch posts based on post type
-        let posts = await News.find();                
+        let posts = await News.find();
         res.status(200).json({
             message: `Retrived ${posts.length} posts of type News`,
             data: posts
         });
-    } catch(error) {
+    } catch (error) {
         return res.status(500).send("An error occured retrieving posts!");
     }
 });
 
-/* Create post */
+/*
+ * @route   GET /posts/news/:id
+ * @desc    Posts routing to get a single News
+ * @access  Public
+ */
+
+// TODO: implement route for single news find
+
+/*
+ * @route   POST /posts
+ * @desc    Posts routing to create a post
+ * @access  Public
+ */
 router.post(
     "/",
     [
@@ -58,38 +78,53 @@ router.post(
 
         try {
             // create post to save to Post db
-            const {post_type} = req.body;
-            let post = new Post({post_type});
+            const { post_type } = req.body;
+            let post = new Post({ post_type });
+
             await post.save();
 
             // create post based on post type and save
+            const {
+                title,
+                description,
+                is_countdown,
+                is_featured,
+                event_date
+            } = req.body;
+
             let newPost;
-            const {title, description, is_featured, event_date} = req.body;
-            switch(post_type) {
+
+            switch (post_type) {
                 case "Event":
                     newPost = new Event({
                         post_id: post,
                         title,
                         description,
                         is_featured,
+                        is_countdown,
                         event_date
                     });
                     break;
                 case "News":
                     newPost = new News({
                         post_id: post,
-                        title,                    
+                        title,
                         description,
                         is_featured
                     });
                     break;
+                default:
+                    return res.status(500).send(`CREATE: No post type match for ${post_type}`);
             }
 
             try {
                 await newPost.save();
-            } catch(error) {
+            } catch (error) {
                 console.error(error.message);
-                // DELETE THE POST
+                Post.findByIdAndDelete(post._id, (err) => {
+                    if (err) console.error(err);
+                    console.log(`Successfully deleted Post id ${post._id} upon failed creation of post type ${post_type}`);
+                });
                 return res.status(500).send(`Error occured creating new post of type ${post_type}!`);
             }
 
@@ -97,41 +132,103 @@ router.post(
                 message: `Your ${post_type} post was successfully created!`,
                 data: newPost
             });
-        } catch(error) {
+        } catch (error) {
             console.error(error.message);
             return res.status(500).send("Error occured creating new post!");
         }
     });
 
-/* Update a Post */
-router.put("/", async (req, res) => {
-    const {id} = req.body.id;
-    try {
-        let postToUpdate = await Post.find({id});
-        // find the post
-        // update the post details
-        // save it
-    } catch(error) {
-        return res.status(500).send("Error saving post!");
-    }
 
-    res.status(200).send("Saving Post Success!")
-});
+/*
+ * @route   PUT /posts/news
+ * @desc    Posts routing to update a News post
+ * @access  Public
+ */
+router.put("/news",
+    [
+        check("title", "A Title is required").not().isEmpty(),
+        check("description", "A description is required").not().isEmpty(),
+        check("post_type", "Post Type is required").not().isEmpty()
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { id, data } = req.body;
+
+        let updatedPost;
+        try {
+            updatedPost = await News.findOneAndUpdate({ _id: id }, data, { new: true });
+        } catch (error) {
+            return res.status(500).send("Error saving news!");
+        }
+
+        res.status(200).send({
+            message: "Saving News Post Success!",
+            data: updatedPost
+        });
+    });
+
+/*
+ * @route   PUT /posts/event
+ * @desc    Posts routing to update an Event post
+ * @access  Public
+ */
+router.put("/event",
+    [
+        check("title", "A Title is required").not().isEmpty(),
+        check("description", "A description is required").not().isEmpty(),
+        check("post_type", "Post Type is required").not().isEmpty()
+    ],
+    async (req, res) => {
+        const { id, data } = req.body;
+
+        let updatedPost;
+        try {
+            updatedPost = await Event.findOneAndUpdate({ _id: id }, data, { new: true });
+        } catch (error) {
+            return res.status(500).send("Error saving event!");
+        }
+
+        res.status(200).send({
+            message: "Saving Event Post Success!",
+            data: updatedPost
+        });
+    });
 
 /* Delete a Post */
 router.delete("/", async (req, res) => {
-    const {id} = req.body.id;
-
+    const { id, post_type, post_id } = req.body;
     try {
-        let postToDelte = await Post.find({id});
-        // delete this post
-        // find the respective post based on post type
-        // delete that post
-    } catch(error) {
+        switch (post_type) {
+            case "Event":
+                await Event.findByIdAndDelete(id, (err) => {
+                    if (err) console.log(err);
+                    console.log("Delete success for type Event")
+                });
+                break;
+            case "News":
+                await News.findByIdAndDelete(id, (err) => {
+                    if (err) console.log(err);
+                    console.log("Delete success for type News")
+                });
+                break;
+            default:
+                return res.status(500).send(`DELETE: No post type match : ${post_type}`);
+        }
+
+        await Post.findByIdAndDelete(post_id, (err) => {
+            if (err) console.log(err);
+            console.log("Delete successful for generic Post");
+        });
+
+        res.status(200).send("Delete Post Success!")
+
+    } catch (error) {
         return res.status(500).send("Error deleting post");
     }
-
-    res.status(200).send("Delete Post Success!")
 });
 
 module.exports = router;
