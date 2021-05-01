562658dcaf4102cf57133f4e1238ca86bd5c4c9f
jihee <developer.jiheuilee@gmail.com>
2021-05-01 15:19:01 -0700

    get to know us and QandAs

--- frontend/src/pages/qanda/index.js
+++ frontend/src/pages/qanda/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container } from "semantic-ui-react";
+import { Container, Image, Grid, Card, Divider } from "semantic-ui-react";
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
 import ContentContainer from "../../components/ContentContainer";
@@ -10,12 +10,12 @@ import './index.css';
 const QandA = () => {
   const qandas = [
     {
-      question: "Question 1", 
-      answer:"Pellentesque tempor urna sapien, at sollicitudin nunc scelerisque in. Nullam odio nibh, rhoncus ut quam sed, porttitor luctus sem. Proin maximus euismod lectus vitae fermentum. Fusce iaculis urna in massa efficitur, id porta felis malesuada. Maecenas odio elit, rutrum in pharetra sed, tristique sit amet est. Suspendisse in hendrerit mauris, ut aliquam quam."
+      question: "Where can I find the ebook?", 
+      answer:"You can download free e-book at https://z-lib.org. try to check out the website and search for the book you need  👍"
     },
     {
-      question: "Question 2", 
-      answer:"Pellentesque tempor urna sapien, at sollicitudin nunc scelerisque in. Nullam odio nibh, rhoncus ut quam sed, porttitor luctus sem. Proin maximus euismod lectus vitae fermentum. Fusce iaculis urna in massa efficitur, id porta felis malesuada. Maecenas odio elit, rutrum in pharetra sed, tristique sit amet est. Suspendisse in hendrerit mauris, ut aliquam quam."
+      question: "Where can I find the good resources for study?", 
+      answer:"You can go to Linkedin Learning and sign up for free with your student account, it has great study materials that are free for the students"
     },
     {
       question: "Question 3", 
@@ -28,13 +28,13 @@ const QandA = () => {
       <Container>
         <ContentContainer>
           <h1>FREQUENTLY ASKED QUESTIONS</h1>
-          <div className="centered-text" id="qanda-content-container">
+          <div id="qanda-content-container">
             {qandas.map((qa, id) =>
               <ContentToggler
                 key="id"
                 title={qa.question}
                 icon="question circle icon">
-                <p>{qa.answer}</p>
+                <p className="answer">{qa.answer}</p>
               </ContentToggler>
             )}
           </div>
